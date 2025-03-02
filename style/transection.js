76abd3d4c90717d98eacc741cbeaecda09e7a132
Atikur-Rahman-Milon14 <milon144900@gmail.com>
2025-03-02 20:21:29 +0600

    color added

--- style/transection.js
+++ style/transection.js
@@ -1,2 +1,11 @@
+const buttoon = document.getElementById('colorChange');
 
+const colors = ['red', 'green', 'blue', 'yellow', 'purple'];
+let colorIndex = 0;
+
+
+buttoon.addEventListener('click',function(){
+    document.body.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1)%colors.length;
+});
 
